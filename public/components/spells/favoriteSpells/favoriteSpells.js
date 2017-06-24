6e78b3f5b7461d1ce645aebf646b5bdb685002bc
Mike Miller <mikem.exe@gmail.com>
2017-06-24 17:10:57 -0600

    My Spells and Favorite Spells working. Spells can be un-favorited from Favorite Spells page, but the page doesn't refresh correctly after that. All the spell names disappear. And spells can't yet be added to favorites from the All Spells page

--- public/components/spells/favoriteSpells/favoriteSpells.js
+++ public/components/spells/favoriteSpells/favoriteSpells.js
@@ -13,29 +13,42 @@ app.controller("FavoriteSpellsCtrl", ["$scope", "$http", "FavoriteSpellsService"
     })();
 
     $scope.unfavoriteSpell = function (user, spell) {
-        FavoriteSpellsService.unfavoriteSpell(user, spell);
+        FavoriteSpellsService.unfavoriteSpell(user, spell).then(function (spells) {
+            console.log("unfavoriteSpells response (favorite spells) :", user);
+            $scope.spells = spells;
+        });
     }
 }]);
 
-app.service("FavoriteSpellsService", ["$http", function ($http) {
+app.service("FavoriteSpellsService", ["$http", "UserService", "$location", function ($http, UserService, $location) {
     this.getFavoriteSpells = function () {
         return $http.get("/api/spells/favorites").then(function (response) {
             console.log("response.data:", response.data)
             return response.data;
         }, function (response) {
-            alert("Error " + response.status + ": " + response.statusText);
+            // If there's any error, get the user to log in.
+            // This should really only happen if the problem is that the user is not logged in.
+            $location.path("/login");
         });
     };
-    this.unfavoriteSpell = function (user, spell) {
+    this.unfavoriteSpell = function (spell) {
         console.log("unfavorite spell");
+
+        var user = UserService.getUser();
         var index = user.favorites.indexOf(spell);
-        console.log("index:", index);
-        if (index > -1) {
-            user.favorites.splice(index, 1);
+        // find the index of the spell
+        for (var i = 0; i < user.favorites.length; i++) {
+            var spellId = user.favorites[i];
+            if (spellId == spell._id) {
+                user.favorites.splice(i, 1);
+                break;
+            }
+            console.log("not found");
         }
 
-        return $http.put("/users", user).then(function (response) {
-            return response.data;
+        return $http.put("/users/" + user._id, user, {new: true}).then(function (response) {
+            UserService.setUser(response.data);
+            return response.data.favorites;
         }, function (response) {
             alert("Error " + response.status + ": " + response.statusText);
         });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,11 @@ var User = require("../models/user");
 
 userRoutes.put("/:id", function (req, res) {
     console.log(req.params.id);
-    User.findOneAndUpdate({_id: req.params.id}, req.body, function (err, existingUser) {
+    console.log("before findOneAndUpdate:", req.body);
+    User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function (err, existingUser) {
         if (existingUser === null) return res.status(500).send("No user found");
         if (err) return res.status(500).send(err);
+        console.log("in put:", existingUser);
         return res.send(existingUser);
     });
 });
