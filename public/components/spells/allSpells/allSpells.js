455df2afcef135169bde4aa2fa2fb7becff18192
Mike Miller <mikem.exe@gmail.com>
2017-06-25 20:30:00 -0600

    Fovorite Spells and All Spells now working. You can press the button below a spell to add it as a favorite, or in the favorites screen, you can press a button to remove it from your favorites.

--- public/components/spells/allSpells/allSpells.js
+++ public/components/spells/allSpells/allSpells.js
@@ -1,6 +1,6 @@
 var app = angular.module("BookOfShadowsApp");
 
-app.controller("AllSpellsCtrl", ["$scope", "$http", "AllSpellsService", function ($scope, $http, SpellsService) {
+app.controller("AllSpellsCtrl", ["$scope", "$http", "SpellsService", function ($scope, $http, SpellsService) {
     $scope.spells = [];
 
     // define and immediately invoke this function when the
@@ -11,9 +11,18 @@ app.controller("AllSpellsCtrl", ["$scope", "$http", "AllSpellsService", function
             $scope.spells = spells;
         });
     })();
+
+
+    $scope.favoriteSpell = function (user, spell) {
+        SpellsService.favoriteSpell(user, spell).then(function (spells) {
+            //Do nothing with spells. It's a list of strings, not the list of spell objects
+        });
+    }
+
+
 }]);
 
-app.service("AllSpellsService", ["$http", function ($http) {
+app.service("SpellsService", ["$http", "UserService", function ($http, UserService) {
     this.getSpells = function () {
         return $http.get("/api/spells").then(function (response) {
             return response.data;
@@ -21,4 +30,31 @@ app.service("AllSpellsService", ["$http", function ($http) {
             alert("Error " + response.status + ": " + response.statusText);
         });
     };
+
+    this.favoriteSpell = function (spell) {
+        console.log("favorite spell");
+
+        var user = UserService.getUser();
+
+        // if the spell is already favorited, remove it from the list and place it at the end.
+        var found = false;
+        for (var i = 0; i < user.favorites.length; i++) {
+            var spellId = user.favorites[i];
+            if (spellId == spell._id) {
+                console.log("user.favorites before slice:", user.favorites);
+                user.favorites.splice(i, 1);
+                console.log("Spliced at ", i);
+                console.log("user.favorites after slice:", user.favorites);
+            }
+        }
+        user.favorites.push(spell);
+
+        return $http.put("/users/" + user._id, user, {new: true}).then(function (response) {
+            UserService.setUser(response.data);
+            return response.data.favorites;
+        }, function (response) {
+            alert("Error " + response.status + ": " + response.statusText);
+        });
+    }
+
 }]);
--- public/components/spells/favoriteSpells/favoriteSpells.js
+++ public/components/spells/favoriteSpells/favoriteSpells.js
@@ -9,13 +9,24 @@ app.controller("FavoriteSpellsCtrl", ["$scope", "$http", "FavoriteSpellsService"
         FavoriteSpellsService.getFavoriteSpells().then(function (spells) {
             console.log(spells);
             $scope.spells = spells;
+            console.log("initially, setting scope.spells to ", spells);
         });
     })();
 
-    $scope.unfavoriteSpell = function (user, spell) {
-        FavoriteSpellsService.unfavoriteSpell(user, spell).then(function (spells) {
-            console.log("unfavoriteSpells response (favorite spells) :", user);
-            $scope.spells = spells;
+    $scope.unfavoriteSpell = function (spell) {
+        self = this;
+        console.log("sent into unfavoriteSpell:", spell);
+        self.spell = spell;
+        FavoriteSpellsService.unfavoriteSpell(spell).then(function (spells) {
+            // Remove the spell from scope.spells so the spell disappears without requiring a refresh
+            for (var i = 0; i < $scope.spells.length; i++) {
+                var theSpell = $scope.spells[i];
+                console.log("theSpell._id: ", theSpell._id);
+                console.log("self.spell._id: ", self.spell._id);
+                if (theSpell._id === self.spell._id) {
+                    $scope.spells.splice(i, 1);
+                }
+            }
         });
     }
 }]);
@@ -35,7 +46,6 @@ app.service("FavoriteSpellsService", ["$http", "UserService", "$location", funct
         console.log("unfavorite spell");
 
         var user = UserService.getUser();
-        var index = user.favorites.indexOf(spell);
         // find the index of the spell
         for (var i = 0; i < user.favorites.length; i++) {
             var spellId = user.favorites[i];
@@ -43,7 +53,6 @@ app.service("FavoriteSpellsService", ["$http", "UserService", "$location", funct
                 user.favorites.splice(i, 1);
                 break;
             }
-            console.log("not found");
         }
 
         return $http.put("/users/" + user._id, user, {new: true}).then(function (response) {
