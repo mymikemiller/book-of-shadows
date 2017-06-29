f600e7cfa2c283bdd254e088a48adfc3a0f19730
Mike Miller <mikem.exe@gmail.com>
2017-06-29 15:06:14 -0600

    Styling the Create Spells page. Also adds the ability to specify an arbitrary number of ingredients

--- public/components/spells/createSpells/createSpells.js
+++ public/components/spells/createSpells/createSpells.js
@@ -1,9 +1,18 @@
 angular.module("BookOfShadowsApp")
 
-.controller("CreateSpellCtrl", ["$scope", "CreateSpellService", function ($scope, CreateSpellService) {
-    $scope.addSpell = function (spell) {
-        CreateSpellService.postNewSpell(spell).then(function (response) {
-            console.log("done");
-        });
-    };
-}]);
+    .controller("CreateSpellCtrl", ["$scope", "CreateSpellService", function ($scope, CreateSpellService) {
+        $scope.addSpell = function (spell) {
+            CreateSpellService.postNewSpell(spell).then(function (response) {
+                console.log("done");
+            });
+        };
+
+        console.log($scope.spell);
+
+        $scope.spell = {ingredients: []};
+
+        $scope.addIngredient = function (ingredient) {
+            $scope.spell.ingredients.push(ingredient);
+        }
+
+    }]);
